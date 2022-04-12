461d85cd965798798cdb73decf83deea282a6069
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-04-12 01:44:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,38 +9,38 @@ let win;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
-  app.quit();
+   // eslint-disable-line global-require
+   app.quit();
 }
 
 const createWindow = () => {
-  // Print out version to console once launched
+   // Print out version to console once launched
 
-  console.log(`Starting up...\nWelcome to ${full_version}!`);
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 750,
-    minWidth: 1155,
-    minHeight: 750,
-    titleBarStyle: "hiddenInset",
-    hasShadow: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-      // devTools: true,
-      // preload: path.join(__dirname, "appFunctions.js"),
-    },
-  });
+   console.log(`Starting up...\nWelcome to ${full_version}!`);
+   // Create the browser window.
+   const mainWindow = new BrowserWindow({
+      width: 1200,
+      height: 750,
+      minWidth: 1155,
+      minHeight: 750,
+      titleBarStyle: "hiddenInset",
+      hasShadow: true,
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false,
+         enableRemoteModule: true,
+         // devTools: true,
+         // preload: path.join(__dirname, "appFunctions.js"),
+      },
+   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(hotreload_base);
+   // and load the index.html of the app.
+   mainWindow.loadFile(hotreload_base);
 
-  // Open the DevTools.
- // mainWindow.webContents.openDevTools();
-  mainWindow.setOpacity(0.95);
-  win = mainWindow;
+   // Open the DevTools.
+   // mainWindow.webContents.openDevTools();
+   mainWindow.setOpacity(0.95);
+   win = mainWindow;
 };
 
 // This method will be called when Electron has finished
@@ -52,45 +52,45 @@ app.on("ready", createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+   if (process.platform !== "darwin") {
+      app.quit();
+   }
 });
 
 app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+   // On OS X it's common to re-create a window in the app when the
+   // dock icon is clicked and there are no other windows open.
+   if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+   }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
 ipc.on("reqPageSwitch", (event, data) => {
-  console.log("Page Switch Requested.");
-  win.loadFile(hotreload_base + "/" + data));
+   console.log("Page Switch Requested.");
+   win.loadFile(hotreload_base + "/" + data);
 });
 
 let currentState;
 
 ipc.on("sendState", (event, data) => {
-  currentState = data;
-  console.log(`currentState = ${currentState}\n`);
+   currentState = data;
+   console.log(`currentState = ${currentState}\n`);
 });
 
 // Send current state (package#)
 // If nothing is saved it will send back nothing at all
 
 ipc.on("reqState", (event, data) => {
-  event.reply("sendState", currentState);
+   event.reply("sendState", currentState);
 });
 
 // Send out both the version number and full version name if requested
 
 ipc.on("reqVer", () => {
-  // win.webContents.send("version", version);
-  win.webContents.send("full_version", full_version);
-  console.log("App wants version");
+   // win.webContents.send("version", version);
+   win.webContents.send("full_version", full_version);
+   console.log("App wants version");
 });
